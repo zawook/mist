20ab78a7160ce6be88da9b5526678551eece5ff4
zawook <zawookmist@gmail.com>
2021-06-17 06:02:19 +0900

    20210615 게시판 테스트용 DB구축 세팅 자습

---
+++ 20210615/DatabaseConfiguration.java
@@ -0,0 +1,28 @@
+package board.configuration;
+import javax.sql.DataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+@PropertySource("classpath:/application.properties")
+public class DatabaseConfiguration {
+	
+	@Bean
+	@ConfigurationProperties(prefix="spring.datasource.hikari")
+	public HikariConfig hikariConfig() {
+		return new HikariConfig();
+	}
+	@Bean
+	public DataSource dataSource() throws Exception {
+		DataSource dataSource = new HikariDataSource(hikariConfig());
+		System.out.println(dataSource.toString());
+		return dataSource;
+		// 게시판용 데이터베이스 설정 자습(2021.06.15)
+	}
+
+}
